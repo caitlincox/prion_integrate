b589e9967e174970230dd66d3d29510360fd0bda
Bill Cox <waywardgeek@google.com>
2024-02-29 12:40:39 -0800

    update

--- prion_integrate.cc
+++ prion_integrate.cc
@@ -1,5 +1,21 @@
-#include "state.h"
+#include "death.h"
+#include "integrator.h"
 
 int main(){
-  
+  /*
+    double lambda = 0.2216;
+    double deathRateInYears = 
+    IntegrationParams integrationParams = {
+        .deltaTime = 1.0  // In days;
+        .deltaLogInfection = ;// Per day growth rate.
+    };
+struct ModelParams {
+    double maxAge;
+    double aveLifespan;
+    double aveInfectiousPeriod; //We derive an intrinsic growth rate based on ave infectious period
+    double aveInitInfectionLoad; //and ave initial infection load. We don't carry over distributional properties
+    //because it's not a terrible assumption that there's an intrinsic prion doubling time. Also, that's super complicated.
+    //Note that if using to *fit* a model, you would want to keep pairs together! Please don't input actual averages from data.
+};
+    //*/
 }
