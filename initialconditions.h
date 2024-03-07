4ccf760ec3f88e463227d7b712e488bde6d3783a
caitlin cox <kittenator22@gmail.com>
2024-03-07 12:12:48 -0800

    update

--- initialconditions.h
+++ initialconditions.h
@@ -10,9 +10,10 @@ public:
     double weibullOfAge(double weibullLambda, double weibullKappa, double age);
     std::unique_ptr<std::vector<double>> startingDistribution(double weibullLambda, double weibullKappa, double maxAge, double deltaT,
                                                               double popSize);
-    std::unique_ptr<std::vector<double>> initialInfecteds(double nunInfecteds, double deltaT, double maxAge, double numInfectionBuckets,
-                                                          double scaleParam, double shapeParam, const std::vector<double>& loadVec);
-
+    std::unique_ptr<std::vector<double>> initialInfecteds(double numInfecteds, double deltaT, double maxAge, double numInfectionBuckets,
+                                                                         double scaleParam, double shapeParam, const std::vector<double>& loadVec,
+                                                                          std::vector<double>& susceptibles, double numSusceptibles, double aveLoad,
+                                                                          double c);
 };
 
 #endif
